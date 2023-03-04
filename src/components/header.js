0b352d96a4c377079e46669da41d88ebfb157bd2
Shaun <seansama@pop-os.localdomain>
2023-03-04 12:20:44 +0300

    Header Component complete

--- src/components/header.js
+++ src/components/header.js
@@ -2,5 +2,32 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 function Header(){
-
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <NavLink className="navbar-brand" to="/">My App</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/" exact>Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/tasks">Tasks</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/newtasks">New Tasks</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/taskdetails">Task Details</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
+
+export default Header
