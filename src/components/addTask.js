306bd9f52433a80614469edd9a4d80d37a20ff6e
Shaun <seansama@pop-os.localdomain>
2023-03-05 12:05:46 +0300

    Styled addTask

--- src/components/addTask.js
+++ src/components/addTask.js
@@ -42,34 +42,47 @@ const AddTask = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Task Name:
-                <input type="text" value={task_name} onChange={handleTaskNameChange} />
-            </label>
-            <br />
-            <label>
-                Description:
-                <input type="text" value={description} onChange={handleDescriptionChange} />
-            </label>
-            <br />
-            <label>
-                Due Date:
-                <input type="date" value={due_date} onChange={handleDueDateChange} />
-            </label>
-            <br />
-            <label>
-                Status:
-                <select value={status} onChange={handleStatusChange}>
-                    <option value="">Select Status</option>
-                    <option value="pending">Pending</option>
-                    <option value="completed">Completed</option>
-                </select>
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
-    );
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+            <div style={{ backgroundColor: "#f1f1f1", borderRadius: "10px", width: "40%", padding: "20px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)" }}>
+                <form onSubmit={handleSubmit} style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}>
+                    <label style={{ marginBottom: "20px" }}>
+                        Task Name:
+                        <br />
+                        <input type="text" value={task_name} onChange={handleTaskNameChange} style={{ width: "100%", padding: "10px", marginTop: "10px" }} />
+                    </label>
+
+                    <label style={{ marginBottom: "20px" }}>
+                        Description:
+                        <br />
+                        <input type="text" value={description} onChange={handleDescriptionChange} style={{ width: "100%", padding: "10px", marginTop: "10px" }} />
+                    </label>
+
+                    <label style={{ marginBottom: "20px" }}>
+                        Due Date:
+                        <br />
+                        <input type="date" value={due_date} onChange={handleDueDateChange} style={{ width: "100%", padding: "10px", marginTop: "10px" }} />
+                    </label>
+
+                    <label style={{ marginBottom: "20px" }}>
+                        Status:
+                        <br />
+                        <select value={status} onChange={handleStatusChange} style={{ width: "100%", padding: "10px", marginTop: "10px" }}>
+                            <option value="">Select Status</option>
+                            <option value="pending">Pending</option>
+                            <option value="completed">Completed</option>
+                        </select>
+                    </label>
+
+                    <button type="submit" style={{ backgroundColor: "#008CBA", color: "white", padding: "10px", borderRadius: "5px", border: "none", marginTop: "20px" }}>Submit</button>
+                </form>
+
+            </div>
+        </div>
+    )
 };
 
 export default AddTask;
